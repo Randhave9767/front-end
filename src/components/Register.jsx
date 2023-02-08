7a68ef5994032b96b9d9c36af22da196d4fa6c15
Randhave9767 <omkarprandhave@gmail.com>
2023-02-08 10:07:16 +0530

    Added register page

---
+++ src/components/Register.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from '@mui/material/TextField';
+import { useNavigate, useRouteError } from "react-router-dom";
+import { useState } from "react";
+
+
+export default function Register()
+{
+    const [name , setName] = useState();
+    const navigate = useNavigate();
+
+    const handleName = (e)=>{
+        setName(e.target.value);
+    }
+
+    const HandleRegister = (e)=>{
+        
+        navigate("Login");
+        
+    }
+    return (
+        <>
+        <h1>{name}</h1>
+        <Box component="form"
+        sx={{
+        '& .MuiTextField-root': {  width: '25ch'},
+        }}
+        
+        >
+        <div className="form_div">
+            <h2>Create an account</h2>
+            <h5>Connect with your friends today!</h5><br/>
+            
+            
+            <TextField id="outlined-basic" label="Username" variant="outlined" onChange={handleName}/><br/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Phone No." variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Password" variant="outlined"/><br/>
+            <Button variant="contained" type="submit" onClick={HandleRegister}>Sign Up</Button>
+            
+        </div>
+        </Box>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
-import Register from './Register';
+import Register from './components/Register';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <Header/>
-    <Register/>
+    <App/>
   </React.StrictMode>
 );
 
